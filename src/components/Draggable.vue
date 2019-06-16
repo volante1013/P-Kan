6788431d6e741e8f61fe1d3973b9861e986bb3ad
volante1013 <u.swb01140@gmail.com>
2019-06-17 07:18:18 +0900

    Draggableコンポーネント追加

---
+++ src/components/Draggable.vue
@@ -0,0 +1,18 @@
+<template>
+	<div id="draggable"></div>
+</template>
+
+<script lang="ts">
+	import { Component, Vue } from 'vue-property-decorator';
+	import draggable from 'vuedraggable';
+
+	@Component({
+		components: {
+			draggable,
+		},
+	})
+	export default class Draggable extends Vue {}
+</script>
+
+<style>
+</style>
